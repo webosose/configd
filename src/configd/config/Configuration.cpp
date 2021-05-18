b6a76be339d710cc9f85899e7364dd4c02204ec3
venugopal.k <venugopal.k@lgepartner.com>
2021-05-18 10:31:22 +0000

    Fixed configd static issue
    :Release Notes:
    Fixed configd static issue
    
    :Detailed Notes:
    Fixed below static issues reported by coverity tool:
    CID-8979183
    
    :Testing Performed:
    Build Successful
    Verified locally
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-143120]Fix configd static issue CID-8979183
    
    Change-Id: Ic5663a4c66e7dc649c0252b612e3c203024cfab3

--- src/configd/config/Configuration.cpp
+++ src/configd/config/Configuration.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2020 LG Electronics, Inc.
+// Copyright (c) 2014-2021 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -334,15 +334,13 @@ bool Configuration::runPostProcess(JsonDB &jsonDB)
     postDB.load(outputFilename);
     jsonDB.copy(postDB);
 
-    // TODO: Do we need to sync here?
-
 Done:
     if (inputFilename != NULL) {
-        g_unlink(inputFilename);
+        (void)g_unlink(inputFilename);
         free(inputFilename);
     }
     if (outputFilename != NULL) {
-        g_unlink(outputFilename);
+        (void)g_unlink(outputFilename);
         free(outputFilename);
     }
     return result;
