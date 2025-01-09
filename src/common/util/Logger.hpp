99873ce1395fa8e1b4dc0d6792192dc78547434a
jothika.selvakumar <jothika.selvakumar@lgepartner.com>
2025-01-09 15:04:15 +0530

    Static issue fix for configd in ose
    :Release Notes:
    Fix static issues for configd
    
    :Detailed Notes:
    Added std::move instead of copy
    CIDs: 12409324
    
    :Testing Performed:
    Build successful
    Test case verified:
    QWTC-10437
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [WRR-16341] Static issues fix in ose
    
    Change-Id: Id4eea52e3b6a9f520a71b41afb90bc75239ec70d

--- src/common/util/Logger.hpp
+++ src/common/util/Logger.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2023 LG Electronics, Inc.
+// Copyright (c) 2017-2025 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -96,7 +96,7 @@ public:
         }
 
         if (type == LogType_File) {
-            if (path.empty() || !setLogFilePath(path)) {
+            if (path.empty() || !setLogFilePath(std::move(path))) {
                 return false;
             }
         }
